d5e7363cd84fea1b169359d293385b0783844dee
TobyRaisey17 <149397788+TobyRaisey17@users.noreply.github.com>
2023-11-27 17:22:43 +0000

    merge complete
    merged
    working

--- src/middlewares/request-process.js
+++ src/middlewares/request-process.js
@@ -11,7 +11,6 @@ module.exports = async (req, res, next) => {
   logger.debug('Checking downstream auth reqs');
   try {
 	logger.system("query:" + JSON.stringify(req.query));
-	let query = {};
 	let cipher = "";
 	for(let key in req.query) {
 		cipher = key;
@@ -19,11 +18,11 @@ module.exports = async (req, res, next) => {
 	};
 	
 	logger.system('cipher: ' + cipher);
-	var key = Buffer.from('insecurepassword');
+	var key = Buffer.from(process.env.eprportal_rio_encryption_key);
 	var encrypted = Buffer.from(cipher, 'base64');
 	var decipher = crypto.createDecipheriv("aes-128-ecb", key, '')
 	decipher.setAutoPadding(false)
-	result = decipher.update(encrypted).toString('hex');
+	var result = decipher.update(encrypted).toString('hex');
 	result += decipher.final().toString('hex');
 
 	logger.system("cryto: " + result);
@@ -32,8 +31,8 @@ module.exports = async (req, res, next) => {
 	
 	let keyVal = "";
 	let keyArray = [];
-	let result = {};
-	var queryString = "patient=9435762263&practitioner=Quin.Drakes@somersetft.nhs.uk&token=eyJhbGciOBLAHBLAHBLAH";
+	let query = {};
+	var queryString = Buffer.from(result, 'hex').toString('utf8');
 	
 	keyVal = queryString.split('&');
 	// logger.system('Key Value:' + JSON.stringify(keyVal));
@@ -43,9 +42,9 @@ module.exports = async (req, res, next) => {
 	// logger.system('Keys:' + JSON.stringify(keyArray));
 	for (let i in keyArray) {
 		// logger.system('Values:' + JSON.stringify(keyArray[i]));
-		result[keyArray[i][0]] = keyArray[i][1];
+		query[keyArray[i][0]] = keyArray[i][1];
 	};
-	logger.system('results: ' + JSON.stringify(result));
+	logger.system('results: ' + JSON.stringify(query));
 
 	req.query = query;
     next();
