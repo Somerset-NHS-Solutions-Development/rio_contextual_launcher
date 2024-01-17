65a998543542329cecd33a09c998c39f8dcb965b
Toby Raisey <Toby.Raisey@SomersetFT.nhs.uk>
2024-01-17 10:34:07 +0000

    Timestamp version 2

--- src/middlewares/query-validator.js
+++ src/middlewares/query-validator.js
@@ -1,23 +1,42 @@
+const { format } = require('winston');
 const logger = require('../utils/logger.js');
 
 
 module.exports = async (req, res, next) => { 
-    req.query["timestamp"] = "2024-01-15T12:46:00.001Z";
-    let parsedTimestamp = Date.parse(req.query["timestamp"])
+    console.log("Timestamp value: " + req.query["timestamp"]);
+    if (req.query["timestamp"].length > 19) {
+        req.query["timestamp"] = req.query["timestamp"].substring(0, 19);
+    }
+    
+    if (req.query["timestamp"].includes("/")) {
+        // Takes day, month + "/", year, and other from timestamp
+        day = req.query["timestamp"].substring(0, 2);
+        month = req.query["timestamp"].substring(2, 6);
+        year = req.query["timestamp"].substring(6, 10);
+        other = req.query["timestamp"].substring(10, 19);
+        let formattedTimestamp = year + month + day + other;
+        formattedTimestamp = formattedTimestamp.replaceAll("/", "-");
+        formattedTimestamp = formattedTimestamp.replace(" ", "T");
+        req.query["timestamp"] = formattedTimestamp;
+    };
+
+
+    let workDate = new Date(req.query["timestamp"]);
+    let parsedTimestamp = Date.parse(workDate);
 	//5 minutes
-	parsedTimestamp += parseInt(process.env.expirationLength)
-	const date = new Date().toJSON();
-	let parsedDate = Date.parse(date)
-	logger.debug("Current Date: " + date + " Timestamp: " + parsedTimestamp)
+	parsedTimestamp += parseInt(process.env.expirationLength);
+	const date = new Date("January 15, 2024 15:00:29").toJSON();
+	let parsedDate = Date.parse(date);
+	logger.debug("Current Date: " + parsedDate + " Timestamp: " + parsedTimestamp);
 
 		
 	if (parsedDate < parsedTimestamp) {		
-        logger.debug("Within Bounds")
-		next();
+        logger.debug("Within Bounds");
+        next();
 	}	
 		
 	else {
-        logger.debug("Out of Bounds")
+        logger.debug("Out of Bounds");
 		res.status(408).send("Token Expired");
 		res.end();
 	}
